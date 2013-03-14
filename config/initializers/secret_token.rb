b99fec62c5aded3b54ae62641167ab2300edd8ec
Ulf Otto <uo@2ebureau.de>
2013-03-14 08:39:10 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OttoDe::Application.config.secret_token = '6c7768478efefd502286f5e8eff7b78fbc3da50262d05bb0b14912e21a5b82ff6558cf385d57e8a59f984b2196157da7fd0bdadeb058bc9a5eae366003c90f3e'
